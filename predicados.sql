b41fdfeee9c19e80648f00e3f6e3f93b94bd8011
andresrcc <andrescordova87@gmail.com>
2012-10-31 09:36:13 -0530

    Update predicados.sql

--- predicados.sql
+++ predicados.sql
@@ -2,7 +2,7 @@
 -- Predicados Difusos en PostgreSQLf
 --
 
-CREATE FUZZY PREDICATE altomu ON 1 .. 5 AS (3,4,5,6);  
-CREATE FUZZY PREDICATE mediomu ON 1 .. 5 AS (2,3,3,5);
-CREATE FUZZY PREDICATE bajomu ON 1 .. 5 AS (0,1,2,4);
+CREATE FUZZY PREDICATE alto ON 1 .. 5 AS (3,4,5,6);  
+CREATE FUZZY PREDICATE medio ON 1 .. 5 AS (2,3,3,5);
+CREATE FUZZY PREDICATE bajo ON 1 .. 5 AS (0,1,2,4);
 
